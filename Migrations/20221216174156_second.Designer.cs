6faa6208723bb03fbdfbd100c8431996e5a3802a
Maryll Castelino <maryllcastelino@gmail.com>
2022-12-16 23:25:46 +0530

    added phone number field and digit count validation

--- Migrations/20221216174156_second.Designer.cs
+++ Migrations/20221216174156_second.Designer.cs
@@ -4,6 +4,7 @@ using EmployeeCRUD.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeCRUD.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221216174156_second")]
+    partial class second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -11,6 +11,9 @@ namespace EmployeeCRUD.Models
         [Display(Name = "Employee Name")]
         public string Name { get; set; }
         public string Designation { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^(\d{10})$", ErrorMessage = "Invalid Phone Number")]
+        public string PhoneNumber { get; set; }
         [DataType(DataType.MultilineText)]
         public string Address { get; set; }
         public DateTime? RecordCreatedOn { get; set; }
