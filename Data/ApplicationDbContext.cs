fcad702492331a188164e75a9930adc292d4ccda
Maryll Castelino <maryllcastelino@gmail.com>
2022-12-16 19:48:36 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EmployeeCRUD.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
